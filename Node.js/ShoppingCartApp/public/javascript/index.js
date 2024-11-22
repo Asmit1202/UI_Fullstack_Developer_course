b6262a89ac0d9a04c33aff808fd2615fffc15832
Asmit1202 <asmitpatil1202@gmail.com>
2024-11-22 18:24:36 +0530

    Update 22 Nov 2024

---
+++ Node.js/ShoppingCartApp/public/javascript/index.js
@@ -0,0 +1,49 @@
+var loadselectedPage = (pagetype) => {
+    var templateUrl = '';
+    switch (pagetype) {
+        case 'homePage':
+            templateUrl = 'Templates/homepage.htm';
+            break;
+        case 'product':
+            templateUrl = 'Templates/product.htm';
+            break;
+        case 'mens':
+            templateUrl = 'Templates/mens.htm';
+            break;
+        case 'womens':
+            templateUrl = 'Templates/womens.htm';
+            break;
+        case 'kids':
+            templateUrl = 'Templates/kids.htm';
+            break;
+    }
+
+    axios.get(templateUrl).then((templtResponse) => {
+        console.log(templtResponse);
+        $("main").html(templtResponse.data);
+
+        loginModelInstance.hide();
+    });
+
+
+}
+
+var loginModelInstance;
+document.addEventListener('DOMContentLoaded', () => {
+    loadselectedPage('homePage');
+
+    loginModelInstance = new bootstrap.Modal('#exampleModal', {});
+});
+
+var MensPage = () => {
+    loadselectedPage('mens');
+}
+
+var WomensPage = () => {
+    loadselectedPage('womens');
+}
+
+var KidsPage = () => {
+    loadselectedPage('kids');
+}
+
